e3e8461f5ba29e22ee6fb7d5f9efd30fa6ef650f
ChaiyoKung <kananek45@gmail.com>
2025-05-14 15:21:50 +0000

    feat: add random string resource for dynamic resource group naming

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
+resource "random_string" "random" {
+  length  = 6
+  special = false
+}
+
 resource "azurerm_resource_group" "example" {
-  name     = "rg-gha-tfo"
+  name     = "rg-gha-tfo-${random_string.random.result}"
   location = "East US"
 }
 
 output "resource_group_id" {
   value = azurerm_resource_group.example.id
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
 }
